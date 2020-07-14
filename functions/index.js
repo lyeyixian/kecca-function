56871a21ed21dfacb1d3928287bd71954733a755
Lye Yi Xian <lyeyx999@hotmail.com>
2020-07-14 19:10:55 +0800

    Validate sign up input

--- functions/index.js
+++ functions/index.js
@@ -78,7 +78,40 @@ app.post("/signup", (req, res) => {
   };
   let token, userId;
 
-  // validation
+  // VALIDATION
+  let errors = {};
+
+  // Email
+  const test = isEmail(newUser.email);
+  if (isEmpty(newUser.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  // Password
+  if (isEmpty(newUser.password)) {
+    errors.password = "Must not be empty";
+  }
+
+  if (newUser.password !== newUser.confirmPassword) {
+    errors.confirmPassword = "Password must match";
+  }
+
+  // Student card
+  if (isEmpty(newUser.studentCard)) {
+    errors.studentCard = "Must not be empty";
+  }
+
+  // Name
+  if (isEmpty(newUser.name)) {
+    errors.name = "Must not be empty";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json(errors);
+  }
+
   db.doc(`/users/${newUser.studentCard}`)
     .get()
     .then((doc) => {
@@ -120,5 +153,23 @@ app.post("/signup", (req, res) => {
       }
     });
 });
+
+// Login
+app.post("/login", (req, res) => {});
+
+// Helper function for validation
+const isEmpty = (string) => {
+  return string.trim() === "";
+};
+
+const isEmail = (email) => {
+  const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (email.match(emailRegEx)) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 exports.api = functions.region("asia-east2").https.onRequest(app);
