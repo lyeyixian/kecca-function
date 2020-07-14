b4acbe95a0e5f01fca56a57db9d73ff8cd94c37d
Lye Yi Xian <lyeyx999@hotmail.com>
2020-07-14 13:46:37 +0800

    Created Sign up route

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,30 @@
+// Firebase
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
+const firebase = require("firebase");
+const firebaseConfig = {
+  apiKey: "AIzaSyCsc9LpTd-ZGCFynkqj0eJuxEt4NJlRqy8",
+  authDomain: "my-kecca.firebaseapp.com",
+  databaseURL: "https://my-kecca.firebaseio.com",
+  projectId: "my-kecca",
+  storageBucket: "my-kecca.appspot.com",
+  messagingSenderId: "352665839619",
+  appId: "1:352665839619:web:5e8aa66d7b366ca7ea0636",
+  measurementId: "G-N0CM7Q9LDL",
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://my-kecca.firebaseio.com",
 });
 
-const express = require("express");
-const app = express();
+firebase.initializeApp(firebaseConfig);
 
+// Express
+const app = require("express")();
+
+// Get all events
 app.get("/events", (req, res) => {
   admin
     .firestore()
@@ -18,17 +33,20 @@ app.get("/events", (req, res) => {
     .get()
     .then((data) => {
       let events = [];
+
       data.forEach((doc) => {
         events.push({
           eventId: doc.id,
           ...doc.data(),
         });
       });
+
       return res.json(events);
     })
     .catch((err) => console.error(err));
 });
 
+// Create an event
 app.post("/event", (req, res) => {
   const newEvent = {
     name: req.body.name,
@@ -47,7 +65,34 @@ app.post("/event", (req, res) => {
     })
     .catch((err) => {
       console.error(err);
+
       return res.status(500).json({ error: "something went wrong" });
+    });
+});
+
+// Sign up
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    name: req.body.name,
+    studentCard: req.body.studentCard,
+    nusnetId: req.body.nusnetId,
+  };
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then((data) => {
+      return res
+        .status(201)
+        .json({ message: `user ${data.user.uid} signed up successfully` });
+    })
+    .catch((err) => {
+      console.error(err);
+
+      return res.status(500).json({ error: err.code });
     });
 });
 
