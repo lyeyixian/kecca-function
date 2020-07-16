85c4281efdd1eb49eb63138197167abd13e222d8
Lye Yi Xian <lyeyx999@hotmail.com>
2020-07-16 18:42:03 +0800

    added some more functions (createCCA, setCurrentUserAsAdmin, fbAuthAdmin, fbAuthUser)

--- functions/index.js
+++ functions/index.js
@@ -4,18 +4,26 @@ const functions = require("firebase-functions");
 const app = require("express")();
 
 // Auth
-const FBAuth = require("./util/fbAuth");
+const { fbAuthUser, fbAuthAdmin } = require("./util/fbAuth");
 
 // Handlers
 const { getAllEvents, createOneEvent } = require("./handlers/events");
-const { signup, login } = require("./handlers/users");
+const { signup, login, setCurrentUserAsAdmin } = require("./handlers/users");
+const { createCCA } = require("./handlers/admin");
 
 // Event route
 app.get("/events", getAllEvents);
-app.post("/event", FBAuth, createOneEvent);
+
+// Admin route
+app.post("/event", fbAuthAdmin, createOneEvent);
+app.post("/cca", fbAuthAdmin, createCCA);
 
 // User route
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/admin", fbAuthUser, setCurrentUserAsAdmin);
+
+// Test
+// app.put("/test", FBAuth, setCurrentUserAsAdmin);
 
 exports.api = functions.region("asia-east2").https.onRequest(app);
