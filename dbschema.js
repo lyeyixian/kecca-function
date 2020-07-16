da901518c43386969a2b404da723b3abaffbfc1c
Lye Yi Xian <lyeyx999@hotmail.com>
2020-07-16 13:02:54 +0800

    Update database schema

--- dbschema.js
+++ dbschema.js
@@ -2,10 +2,32 @@ let db = {
   events: [
     {
       name: "Tuesday training",
-      organiser: "basketball",
+      cca: "basketball",
+      organiser: "A0200813X",
       createdAt: "2020-07-13T16:18:07.601Z",
       duration: 2,
-      listOfAttendees: ["Lye Yi Xian", "Tan Xi Ze"],
+      listOfAttendees: ["A0200813X", "A1284973X"],
+      listOfAbsentees: ["A8476298X"],
+    },
+  ],
+  users: [
+    {
+      createdAt: "2020-07-14T10:18:06.949Z",
+      email: "lyeyx999@hotmail.com",
+      name: "Lye Yi Xian",
+      studentCard: "A0200813X",
+      userId: "WX3NbazroKOaye3grRA4AV9X8Xs1",
+      adminStatus: {
+        tokenHeader: "Admin ", // 'User '
+        cca: "basketball", // ''
+      },
+    },
+  ],
+  cca: [
+    {
+      name: "basketball",
+      listOfMembers: ["A0200713X", "A1294857X"],
+      admin: "A0200813X",
     },
   ],
 };
